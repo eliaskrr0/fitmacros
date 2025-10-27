a19c005f13b7620edf1a8548d6dfe1719495b061
eliaskrr0 <developer.elias.essadak@gmail.com>
2025-10-27 19:57:23 +0100

    refactor: color tema app

--- app/src/main/java/com/eliaskrr/fitmacros/ui/theme/Color.kt
+++ app/src/main/java/com/eliaskrr/fitmacros/ui/theme/Color.kt
@@ -6,22 +6,22 @@ import androidx.compose.ui.graphics.Color
 val Brand = Color(0xFF43A047)
 
 // --- Tema Claro ---
-val md_theme_light_primary = Brand
-val md_theme_light_onPrimary = Brand
-val md_theme_light_secondary = Brand
-val md_theme_light_background = Brand // Fondo general
-val md_theme_light_surface = Brand // Blanco puro para las tarjetas
-val md_theme_light_onBackground = Brand // Texto principal (casi negro)
-val md_theme_light_onSurface = Brand // Texto en tarjetas
+val md_theme_light_primary = Color(0xFFFDFDFD) // Borde superior e inferior
+val md_theme_light_onPrimary = Color(0xFF000000) // Texto barra superior
+val md_theme_light_secondary = Color(0xFFFDFDFD)
+val md_theme_light_background = Color(0xFFFDFDFD) // Borde internp
+val md_theme_light_surface = Color(0xFF000000) // Blanco puro para las tarjetas
+val md_theme_light_onBackground = Color(0xFF000000)  // Texto principal (casi negro)
+val md_theme_light_onSurface = Color(0xFF000000) // Texto barra inferior
 
 // --- Tema Oscuro ---
-val md_theme_dark_primary = Color(0xFF82B1FF) // Un azul más claro para el modo oscuro
-val md_theme_dark_onPrimary = Color(0xFF00275B)
-val md_theme_dark_secondary = Brand // La línea que faltaba
-val md_theme_dark_background = Color(0xFF121212) // Fondo oscuro estándar de Material
-val md_theme_dark_surface = Color(0xFF1E1E1E) // Superficie ligeramente más clara
-val md_theme_dark_onBackground = Color(0xFFE4E2E6)
-val md_theme_dark_onSurface = Color(0xFFE4E2E6)
+val md_theme_dark_primary = Brand // Un azul más claro para el modo oscuro
+val md_theme_dark_onPrimary = Brand
+val md_theme_dark_secondary = Brand // La línea que fastball
+val md_theme_dark_background = Brand // Fondo oscuro estándar de Material
+val md_theme_dark_surface = Brand // Superficie ligeramente más clara
+val md_theme_dark_onBackground = Brand
+val md_theme_dark_onSurface = Brand
 
 
 // Colores para los Macronutrientes (acordes al nuevo tema)
