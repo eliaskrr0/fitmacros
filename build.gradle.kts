c7ac2d313e9c4b7fd103c9516c44d5873dc07f1f
eliaskrr0 <104264585+eliaskrr0@users.noreply.github.com>
2025-11-04 19:51:34 +0100

    Forzar versión de Javapoet para tareas de Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,24 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.squareup:javapoet:1.13.0")
+    }
+}
+
 plugins {
-	alias(libs.plugins.android.application) apply false
-	alias(libs.plugins.kotlin.android) apply false
-	alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+}
+
+subprojects {
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "com.squareup" && requested.name == "javapoet") {
+                useVersion("1.13.0")
+                because("Hilt requiere ClassName.canonicalName() disponible desde Javapoet 1.13.0")
+            }
+        }
+    }
 }
