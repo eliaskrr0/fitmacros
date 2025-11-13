13567ee39a9183502c1d0a29d62cded9caacf7e2
eliaskrr0 <104264585+eliaskrr0@users.noreply.github.com>
2025-11-13 18:52:41 +0100

    Store enum converters by name

--- app/src/main/java/com/eliaskrr/fitmacros/data/db/Converter.kt
+++ app/src/main/java/com/eliaskrr/fitmacros/data/db/Converter.kt
@@ -6,14 +6,26 @@ import com.eliaskrr.fitmacros.data.entity.nutrition.type.QuantityUnit
 
 class Converter {
     @TypeConverter
-    fun fromMealType(value: MealType): Int = value.ordinal
+    fun fromMealType(value: MealType): String = value.name
 
     @TypeConverter
-    fun toMealType(value: Int): MealType = MealType.values()[value]
+    fun toMealType(value: String): MealType {
+        val entries = MealType.values()
+        entries.firstOrNull { it.name == value }?.let { return it }
+
+        val ordinal = value.toIntOrNull()
+        return ordinal?.let { entries.getOrNull(it) } ?: entries.first()
+    }
 
     @TypeConverter
-    fun fromQuantityUnit(value: QuantityUnit): Int = value.ordinal
+    fun fromQuantityUnit(value: QuantityUnit): String = value.name
 
     @TypeConverter
-    fun toQuantityUnit(value: Int): QuantityUnit = QuantityUnit.values()[value]
+    fun toQuantityUnit(value: String): QuantityUnit {
+        val entries = QuantityUnit.values()
+        entries.firstOrNull { it.name == value }?.let { return it }
+
+        val ordinal = value.toIntOrNull()
+        return ordinal?.let { entries.getOrNull(it) } ?: entries.first()
+    }
 }
